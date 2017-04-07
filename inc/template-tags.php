32f1d1b10d77704b1b7b5f524b9a0c287a0fe709
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-04-07 16:07:10 +0100

    Updated the template tags after the global variable was renamed to $og_embed_data.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,43 +1,43 @@
 <?php
 
 function opengraph_embed_classes() {
-	global $oge_embed;
+	global $og_embed_data;
 
-	$classes = array( 'opengraph-embed', sanitize_title( $oge_embed['site_name'] ) );
+	$classes = array( 'opengraph-embed', sanitize_title( $og_embed_data['site_name'] ) );
 
-	if ( array_key_exists( 'locale', $oge_embed ) ) {
-		$classes[] = str_replace( '_', '-', sanitize_title( $oge_embed['locale'] ) );
+	if ( array_key_exists( 'locale', $og_embed_data ) ) {
+		$classes[] = str_replace( '_', '-', sanitize_title( $og_embed_data['locale'] ) );
 	}
 
-	if ( array_key_exists( 'type', $oge_embed ) ) {
-		$classes[] = str_replace( '_', '-', sanitize_title( $oge_embed['type'] ) );
+	if ( array_key_exists( 'type', $og_embed_data ) ) {
+		$classes[] = str_replace( '_', '-', sanitize_title( $og_embed_data['type'] ) );
 	}
 
 	printf( ' class="%1$s"', join( ' ', $classes ) );
 }
 
 function opengraph_the_content() {
-	global $oge_embed;
-	echo( $oge_embed['description'] );
+	global $og_embed_data;
+	echo( $og_embed_data['description'] );
 }
 
 function opengraph_the_permalink() {
-	global $oge_embed;
-	echo( $oge_embed['url'] );
+	global $og_embed_data;
+	echo( $og_embed_data['url'] );
 }
 
 function opengraph_the_site_name() {
-	global $oge_embed;
-	echo( $oge_embed['site_name'] );
+	global $og_embed_data;
+	echo( $og_embed_data['site_name'] );
 }
 
 function opengraph_the_thumnbnail() {
-	global $oge_embed;
+	global $og_embed_data;
 
-	printf( '<img src="%1$s" />', $oge_embed['image'] );
+	printf( '<img src="%1$s" />', $og_embed_data['image'] );
 }
 
 function opengraph_the_title() {
-	global $oge_embed;
-	echo( $oge_embed['title'] );
+	global $og_embed_data;
+	echo( $og_embed_data['title'] );
 }
