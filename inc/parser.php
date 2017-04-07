daa33e7e0a894d419b6b4e20c0ae8961c482cb6a
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-04-07 13:28:02 +0100

    Changed the OpenGraph parser to use DOMDocument object instead of regular expressions. Too much variance between websites for a regular expression to work and be sane.

--- inc/parser.php
+++ inc/parser.php
@@ -1,16 +1,38 @@
 <?php
 
 function oge_get_opengraph_data( $html ) {
-	$matches = array();
+	/**
+	 * Effectively silence the errors so that the code does not bomb out at this
+	 * point and continues to work. Despite the oddities through HTML of
+	 * websites across the internet ...
+	 */
+	$old_libxml_error = libxml_use_internal_errors(true);
+
+	$doc = new DOMDocument();
+	$doc->loadHTML( $html );
+
+	libxml_use_internal_errors( $old_libxml_error );
+
+	/**
+	 * Grab all <meta> elements in the HTML response and make sure we have more
+	 * than one element to work with.
+	 */
+	$tags = $doc->getElementsByTagName( 'meta' );
+
+	if ( false === $tags || $tags->length === 0) {
+		return false;
+	}
+
+	/**
+	 * Loop through each metadata tag and construct an dictionary containing the
+	 * data from OpenGraph.
+	 */
 	$opengraph = array();
 
-	preg_match_all('~<\s*meta\s+property="(og:[^"]+)"\s+content="([^"]*)~i', $html, $matches);
-var_dump( $matches );
-	$keys = $matches[1];
-	$values = $matches[2];
-
-	foreach ( $keys as $i => $value ) {
-		$opengraph[str_ireplace( 'og:', '', $value )] = $values[$i];
+	foreach ( $tags as $tag ) {
+		if ( $tag->hasAttribute( 'property' ) && 0 === strpos( $tag->getAttribute( 'property' ), 'og:' ) ) {
+			$opengraph[str_ireplace( 'og:', '', $tag->getAttribute( 'property' ) )] = $tag->getAttribute( 'content' );
+		}
 	}
 
 	return $opengraph;
